0eefe5b9bcc1e359ad60bf29f38973270c188197
NathanielBirhanu <nbirhanu98@gmail.com>
2025-02-11 11:29:12 +0300

    added new file and some modification

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from  flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config('SECRET_KEY') = ''
 post = [
     {
-        "autor": "Natnael Birhanu",
+        "author": "Natnael Birhanu",
         "title": "blog post 1",
-        "content": "This is Nate's post",
-        "date_posted": "today"
+        "content": "This is Natnael's post",
+        "date_posted": "oct 23, 2024"
     },
     {
-        "autor": "Tom Cruise",
+        "author": "Tom Cruise",
         "title": "blog post 2",
         "content": "This is Tom's post",
-        "date_posted": "today"
+        "date_posted": "sept 08, 2024"
     }
 ]
 @app.route("/")
@@ -23,5 +25,13 @@ def home():
 def about():
     return render_template('about.html', title ='About')
 
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title ='Register',form=form)
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title ='Login',form=form)
 if __name__ == "__main__":
     app.run(debug=True)
